b9dc22215365615ddca5c29b01ac8965d4563661
andylindemann <pz81ndl>
2013-04-28 20:18:03 +0200

    dummyjson created

---
+++ dummyjson.js
@@ -0,0 +1,54 @@
+var dummyjson = {
+    colorID: {
+        color: '#ff8000',
+        circles: [
+            {radius: 10,
+                directions: {
+                    a: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    b: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    c: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    }
+
+                }},
+            {radius: 20,
+                directions: {
+                    a: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    b: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    c: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    }
+
+                }},
+            {radius: 30,
+                directions: {
+                    a: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    b: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    },
+                    c: {offset: 0,
+                        pro: 0,
+                        contra: 0
+                    }
+                }}
+        ]
+    }
+}
